42cbf667467df59e9712d538cb7e8743049b00cc
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-10-11 15:50:13 +0700

    Add GlobalNavBar's props

--- src/components/GlobalNavBar/index.js
+++ src/components/GlobalNavBar/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './GlobalNavBar.css';
 import logo from '../../assets/logo.svg';
 
-const GlobalNavBar = () => {
+const GlobalNavBar = (props) => {
   return (
-    <nav className="global-nav sticky top-0">
+    <nav
+      className={`global-nav ${!!props.disableSticky ? '' : 'sticky top-0'}`}
+    >
       <div className="hidden md:flex max-w-screen-lg px-5 mx-auto  flex-row text-white text-sm font-extralight justify-between items-center">
         <a>
           <img src={logo} />
