3d7c96ed0f7740f150518b4912f96113fd77c988
silkk-nn <70426262+silkk-nn@users.noreply.github.com>
2021-10-11 21:17:14 +0700

    add bg help

--- src/pages/help/index.js
+++ src/pages/help/index.js
@@ -6,8 +6,19 @@ const Help = () => {
     return (
         <>
             <GlobalNavBar />
+            <img
+                src="assets\src_image\help\psp-hero-banner-homepage-welcome.image.large_2x.jpg"
+                className="hidden md:block mx-auto"
+                style={{ maxWidth: '1050px' }}
+            />
+            <img
+                src="assets\src_image\help\psp-hero-banner-homepage-welcome.image.small_2x.jpg"
+                className="hidden md:block mx-auto"
+                style={{ maxWidth: '300px' }}
+            />
             <div>
                 <h1>Help</h1>
+                <h2>Help</h2>
             </div>
             <Footer />
         </>
