39f8b2dc47bbeb00b515684d46816c567492160a
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-10-11 16:57:28 +0700

    Add iPhone13Pro Page

---
+++ src/pages/Iphone13Pro/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+import GlobalNavBar from '../../components/GlobalNavBar';
+
+const Iphone13Pro = () => {
+  return (
+    <>
+      <GlobalNavBar />
+      <div>
+          <h1>iPhone 13 Pro</h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Iphone13Pro;
