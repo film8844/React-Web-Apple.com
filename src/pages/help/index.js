0667c3211f5949870ee43d1a753c6386cc153c3c
tunlaton <tunlaton11@gmail.com>
2021-10-11 17:12:05 +0700

    add help page

---
+++ src/pages/help/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+import GlobalNavBar from '../../components/GlobalNavBar';
+
+const Help = () => {
+    return (
+        <>
+            <GlobalNavBar />
+            <div>
+                <h1>Help</h1>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default Help;
