53467a3afaf8d27f3387de3b771e3d4787ddcbfe
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-10-11 16:10:55 +0700

    Home

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
-import GlobalNavBar from './components/GlobalNavBar';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <>
-      <GlobalNavBar />
-      <div>
-        <div className="mt-20 text-center">
-          <h2 className="text-5xl font-bold">iPhone 13 Pro</h2>
-          <p className="text-3xl mt-2">โปรสมชื่อ</p>
-        </div>
-        <div className="mt-5 text-center text-xl text-blue-600">
-          <a className="mr-4">ดูเพิ่มเติม</a>
-          <a>ซื้อ</a>
-        </div>
-        <img
-          src="/assets/hero_iphone13pro.jpg"
-          className="hidden md:block mx-auto px-5 mt-10"
-          style={{ maxWidth: '600px' }}
-        />
-        <img
-          src="/assets/hero_iphone13pro_small.jpg"
-          className="block md:hidden mx-auto px-5 mt-10"
-          style={{ maxWidth: '300px' }}
-        />
-      </div>
+      <Home/>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
