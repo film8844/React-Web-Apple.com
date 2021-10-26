7c1c9e642f14e0daa732ffb87c42e578ce5e5776
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-10-26 16:45:37 +0700

    Update Bag

--- src/pages/Bag/index.js
+++ src/pages/Bag/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppleButton from '../../components/AppleButton';
 import Footer from '../../components/Footer';
 import GlobalNavBar from '../../components/GlobalNavBar';
 import { useStore } from '../../store/store';
@@ -10,14 +11,19 @@ const Bag = () => {
   return (
     <>
       <GlobalNavBar />
-      <div className="max-w-screen-lg mx-auto px-5 py-16">
-        <h2 className=" font-semibold text-4xl text-center">
-          นี่คือรายการสินค้าที่อยู่ในถุงของคุณ
-        </h2>
-        <p className="text-center text-gray-700 mt-5">
-          รับบริการจัดส่งฟรีและส่งคืนฟรีทุกคำสั่งซื้อ
-        </p>
-        <div className="divide-y">
+      <div className="max-w-screen-lg mx-auto px-5">
+        <div className="divide-y divide-gray-300">
+          <div className="my-20">
+            <h2 className=" font-semibold text-4xl text-center">
+              นี่คือรายการสินค้าที่อยู่ในถุงของคุณ
+            </h2>
+            <p className="text-center text-gray-700 mt-5">
+              รับบริการจัดส่งฟรีและส่งคืนฟรีทุกคำสั่งซื้อ
+            </p>
+            <AppleButton className="px-20 text-xl block mx-auto mt-8">
+              ชำระเงิน
+            </AppleButton>
+          </div>
           {!!bag && bag.map((item) => <Item item={item} key={item.id} />)}
         </div>
       </div>
