b912f10943efb92526be33fc213ecc8bf486ff86
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-10-26 19:17:02 +0700

    Remove Unused File

--- src/components/ProductNavBar/index.js
+++ src/components/ProductNavBar/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ProductNavBar.css';
-import logo from '../../assets/logo.svg';
 
 const ProductNavBar = (props) => {
   return (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Footer from '../../components/Footer';
 import GlobalNavBar from '../../components/GlobalNavBar';
 import './index.css';
-import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 
 const Iphone13Pro = () => {
