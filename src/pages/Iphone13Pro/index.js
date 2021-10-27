c82a6deb8d26dadab0de69f147263ccfcc52e3f8
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-10-27 16:01:16 +0700

    Update i13Pro

--- src/pages/Iphone13Pro/index.js
+++ src/pages/Iphone13Pro/index.js
@@ -128,6 +128,14 @@ const Iphone13Pro = () => {
     <>
       <GlobalNavBar disableSticky={true} />
       <ProductNavBar productName="iPhone 13 Pro" priceText={priceText} />
+      <div className="bg-gray-100">
+        <div className="max-w-screen-lg m-auto">
+          <p className="text-sm text-center px-5 py-3">
+            มีการผ่อนชำระในอัตราดอกเบี้ย 0% นานสูงสุด 10 เดือน{' '}
+            <span className="text-blue-600">ดูเพิ่มเติม</span>
+          </p>
+        </div>
+      </div>
       <div className="max-w-screen-lg mx-auto px-5 min-h-screen">
         <div
           id="iphone13pro-selection"
@@ -492,9 +500,23 @@ const Iphone13Pro = () => {
               }`}
             >
               <div className="absolute bg-white bg-opacity-50 h-full w-full"></div>
-              <button className="w-full bg-blue-500 hover:bg-blue-600 text-xl text-white rounded-lg p-1">
-                ต่อไป
-              </button>
+              <div className="flex">
+                <AppleButton className="w-full text-xl">ต่อไป</AppleButton>
+                <svg
+                  viewBox="0 0 35 35"
+                  role="img"
+                  aria-hidden="true"
+                  width="35px"
+                  height="35px"
+                  className="ml-5"
+                >
+                  <path fill="none" d="M0 1.213h35v35H0z"></path>
+                  <path
+                    fill="#0071e3"
+                    d="M17.5 29.263a1.194 1.194 0 01-.85-.352L6.967 19.2a6.531 6.531 0 01.46-9.617 6.708 6.708 0 018.943.647l1.13 1.133 1.13-1.133a6.706 6.706 0 018.943-.647 6.531 6.531 0 01.46 9.617l-9.683 9.711a1.192 1.192 0 01-.85.352zm-6.06-20a5.154 5.154 0 00-3.31 1.167 5.352 5.352 0 00-1.948 3.913 5.424 5.424 0 001.564 4.081l9.684 9.711.53.388-.39-.388 9.684-9.711a5.424 5.424 0 001.564-4.081 5.352 5.352 0 00-1.948-3.913 5.6 5.6 0 00-7.461.577l-1.7 1.7a.3.3 0 01-.425 0l-1.7-1.7a5.89 5.89 0 00-4.144-1.741z"
+                  ></path>
+                </svg>
+              </div>
             </div>
             <div
               className={`p-5 mt-16 divide-y divide-gray-300 ${
@@ -520,7 +542,19 @@ const Iphone13Pro = () => {
                 </p>
               </div>
               <div className="pt-5">
-                <div className="mb-4">
+                <div className="mb-4 flex gap-2">
+                  <div>
+                    <svg
+                      viewBox="0 0 25 25"
+                      role="img"
+                      aria-hidden="true"
+                      width="25px"
+                      height="25px"
+                    >
+                      <path fill="none" d="M0 0h25v25H0z"></path>
+                      <path d="M19.69 7.154l-6-3.245a2.5 2.5 0 00-2.38 0l-6 3.245A2.5 2.5 0 004 9.354v6.269a2.5 2.5 0 001.311 2.2l6 3.245a2.5 2.5 0 002.379 0l6-3.245a2.5 2.5 0 001.31-2.2V9.354a2.5 2.5 0 00-1.31-2.2zm-7.9-2.365a1.492 1.492 0 011.427 0l6 3.244a1.454 1.454 0 01.151.11l-2.931 1.665-6.743-3.886zM8.661 6.48l6.768 3.9-2.929 1.666-6.864-3.9a1.456 1.456 0 01.151-.11zM5.787 16.941A1.5 1.5 0 015 15.622V9.354a1.5 1.5 0 01.053-.39L12 12.912v7.358a1.463 1.463 0 01-.213-.083zM20 15.622a1.5 1.5 0 01-.786 1.319l-6 3.245a1.5 1.5 0 01-.214.084v-7.358l6.947-3.949a1.508 1.508 0 01.053.391z"></path>
+                    </svg>
+                  </div>
                   <p className="text-sm leading-none">
                     <span className="font-bold">การจัดส่ง:</span>
                     <br />
@@ -531,7 +565,20 @@ const Iphone13Pro = () => {
                     <span className="text-blue-500">ดูวันส่งมอบสินค้า</span>
                   </p>
                 </div>
-                <div className="mb-8">
+                <div className="mb-8 flex gap-2">
+                  <div>
+                    <svg
+                      viewBox="0 0 25 25"
+                      role="img"
+                      aria-hidden="true"
+                      width="25px"
+                      height="25px"
+                    >
+                      <path fill="none" d="M0 0h25v25H0z"></path>
+                      <path d="M18.5 5h-1.775a4.231 4.231 0 00-8.45 0H6.5A2.5 2.5 0 004 7.5v11A2.5 2.5 0 006.5 21h12a2.5 2.5 0 002.5-2.5v-11A2.5 2.5 0 0018.5 5zm-6-3a3.245 3.245 0 013.225 3h-6.45A3.245 3.245 0 0112.5 2zM20 18.5a1.5 1.5 0 01-1.5 1.5h-12A1.5 1.5 0 015 18.5v-11A1.5 1.5 0 016.5 6h12A1.5 1.5 0 0120 7.5z"></path>
+                      <path d="M14.4 12.448a1.592 1.592 0 01.738-1.328 1.607 1.607 0 00-1.37-.687c-.52 0-.941.317-1.22.317s-.663-.3-1.129-.3a1.861 1.861 0 00-1.739 2.068 4.32 4.32 0 00.723 2.3c.346.491.648.883 1.084.883s.617-.287 1.144-.287c.55 0 .663.279 1.137.279s.791-.43 1.084-.853a3.24 3.24 0 00.474-.989 1.516 1.516 0 01-.926-1.403zM12.583 10.357a1.346 1.346 0 00.941-.5 1.594 1.594 0 00.361-.974.731.731 0 00-.008-.136 1.5 1.5 0 00-1.016.528 1.547 1.547 0 00-.384.943c0 .053.008.106.008.128.03.004.06.011.098.011z"></path>
+                    </svg>
+                  </div>
                   <p className="text-sm leading-none">
                     <span className="font-bold">การรับสินค้า:</span>
                     <br />
